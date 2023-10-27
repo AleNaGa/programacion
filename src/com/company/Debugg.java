d7fd994ab78b1b0bf958932a1f59d5bdc48257c8
K1MP4 <alextiloup@gmail.com>
2023-10-27 16:55:32 +0200

    Casa debugg

---
+++ src/com/company/Debugg.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Debugg {
+    public static void main (String []args){
+        System.out.println("EJ DEBUGG");
+        for (int i =0; i<10; i++){
+            System.out.println("");
+            for (int j = 0; j<5;j++){
+                System.out.println(i +" " + j);
+            }
+        }
+    }
+}
