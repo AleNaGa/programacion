00208a289ac841604908fe1952348940d49d53b8
K1MP4 <alextiloup@gmail.com>
2023-11-16 15:17:32 +0100

    Create Problemas1611.java

---
+++ src/com/company/Problemas1611.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Problemas1611 {
+    public static void main (String[]args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\nEjercicio 7");
+        // La edad de una persona calcula el año de nacimiento y el horóscopo chino
+        System.out.println("Introduce tu edad: ");
+        int edad = scan.nextInt();
+        int añoNac = 2023-edad;
+        
+
+    }
+}
